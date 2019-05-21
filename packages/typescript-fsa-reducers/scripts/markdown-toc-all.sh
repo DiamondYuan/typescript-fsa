c1b1a00f6dd5d2fcdeac4a0b090f3bc6c2cfa1c4
DiamondYuan <admin@diamondyuan.com>
2019-05-21 18:35:00 +0800

    chore: clean packages.json

--- packages/typescript-fsa-reducers/scripts/markdown-toc-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Needed for lint-staged if multiple .md files are changed since markdown-toc
-# only accepts a single file argument.
-for var in "$@"
-do
-    yarn markdown-toc -i "$var"
-done
